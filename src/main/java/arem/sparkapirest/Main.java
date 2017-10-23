dc48903a6383dfe6a0e51f43a5f1402bc3631543
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2017-10-23 00:32:08 -0500

    Juan Pablo

--- src/main/java/arem/sparkapirest/Main.java
+++ src/main/java/arem/sparkapirest/Main.java
@@ -9,23 +9,41 @@ package arem.sparkapirest;
  *
  * @author JuanArevaloMerchan
  */
+import java.util.HashMap;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 import static spark.Spark.*;
 
 public class Main {
 
+    private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+
+    static {
+        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        corsHeaders.put("Access-Control-Allow-Origin", "*");
+        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+    }
+
+    public final static void apply() {
+        Filter filter = new Filter() {
+            @Override
+            public void handle(Request request, Response response) throws Exception {
+                corsHeaders.forEach((key, value) -> {
+                    response.header(key, value);
+                });
+            }
+        };
+        Spark.after(filter);
+    }
+
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
+        Main.apply();
         get("/cuadrado/:id", (req, res) -> {
             String id = req.params(":id");
-            String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-
-            String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
 
             try {
                 long numero = Long.parseLong(id);
