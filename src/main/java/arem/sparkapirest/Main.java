cf32dd4237ff9fee5d18ddeb1533214ac016e06b
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2017-10-23 00:18:18 -0500

    Juan Pablo

--- src/main/java/arem/sparkapirest/Main.java
+++ src/main/java/arem/sparkapirest/Main.java
@@ -14,6 +14,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         get("/cuadrado/:id", (req, res) -> {
             String id = req.params(":id");
             try{
@@ -26,6 +27,14 @@ public class Main {
             }
         });
     } 
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+}
     
 
 }
